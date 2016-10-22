0678bc12ac3af6da59e5e2280d54690fa195783a
Matthew Page <matthewpagecs@gmail.com>
2016-10-22 09:47:14 -0400

    Pseudocode

--- src/com/nullprogram/chess/you/AlphaBetaPruningAI.java
+++ src/com/nullprogram/chess/you/AlphaBetaPruningAI.java
@@ -15,19 +15,18 @@ public class AlphaBetaPruningAI implements Player {
     }
 
     public MoveScore predictBestMove(int ply, int finalPly, Board board, Piece.Side side) {
-        // If we are on the finalPly
-            // Return an evaluation of the board
-        // else
-            // For each possible move for the current player
-                // Make the move
-                // Recursively call predictBestMove(...)
-                // Undo the move
-            // Return the best move found (for the current player).
-
-        // Hints:
-            // Be sure to reverse the score when you return the MoveScore in the 'else' statmente
-            // Be sure to increment the ply and switch sides when you recursively call predictBestMove(...)
-            // Add the code to keep track of the best MoveScore within the foreach loop.
+        // TODO:
+            // 1. Copy your minimax code to this method
+            // 2. Modify MoveScore to contain the alpha and beta values
+            // 3. Handle the passing of alpha and beta from the parent to the child, and from the child to the parent.
+                // Hint: Modify the method signature for predictBestMove(...)
+                // Hint: When passing to child, multiply alpha and beta by -1 and reverse them
+                // Hint: When passing to parent, do the same as you would for the child, but compare the new alpha/beta
+                //       values against the old ones, and accept the most restrictive ones.
+            // 4. Add the code for initializing Alpha and Beta in the first time you call predictBestMove(...)
+            // 5. Add the code for pruning away sections of the search tree you don't want to explore
+                // Hint: When alpha and beta contradict one another, your prune away that node. This will only happen
+                //       after a node's child has been analized.
 
         return null;
     }
